14910723bb2660a547733cbe726fdafd2bbb5566
fconstancio <constanciosalomao@gmail.com>
2021-05-06 17:36:46 -0300

    correção e saída opcional java-lista4-exercicio4

--- java/AULAS/src/exercicios/lista3/Exercicio4.java
+++ java/AULAS/src/exercicios/lista3/Exercicio4.java
@@ -5,50 +5,57 @@ public class Exercicio4 {
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
 		
-		final int AMOSTRA = 5;
+		final int AMOSTRA = 150;
 		int count = 1;
-		int idade, sexo, opcao;
-		int countCalm,countMn, countHa, countOc, countNq, countCd;
-		countCalm = countMn =  countHa =  countOc = countNq = countCd = 0;
+		boolean continua = true;
+		int idade;
+		char sexo, opcao;
+		int countC = 0;		//Calm
+		int countFn = 0;	//FeminioNervoso	
+		int countMa =0;		//MasculinoAgressivo
+		int countOc = 0;	//OutroCalmo
+		int countNq = 0;	//NervosoQuarenta
+		int countCd = 0;	//CalmoDezoito
 		
-		while(count < AMOSTRA) {
-			System.out.println("INDIVÍDUO: " + count);
+		while(continua  && count < AMOSTRA) {
+			
+			System.out.println("\nINDIVÍDUO: " + count);
 			System.out.printf("Idade: ");
 			idade = sc.nextInt();
 			System.out.printf("1-Feminino / 2-Masculino / 3- Outros: ");
-			sexo = sc.nextInt();
-			System.out.printf("1-Calma / 2-Nervosa / 3- Agressiva");
-			opcao = sc.nextInt();
+			sexo = sc.next().charAt(0);
+			System.out.printf("1-Calma / 2-Nervosa / 3- Agressiva: ");
+			opcao = sc.next().charAt(0);
 			
-			if (opcao == 1) {
-				countCalm++;
+			if (opcao == '1') {
+				countC++;
 			}
-			if (sexo == 1 && opcao == 2) {
-				countMn++;
+			if (sexo == '1' && opcao == '2') {
+				countFn++;
 			}
-			if (sexo == 2 && opcao == 3) {
-				countHa++;
+			if (sexo == '2' && opcao == '3') {
+				countMa++;
 			}
-			if (sexo == 3 && opcao == 1) {
+			if (sexo == '3' && opcao == '1') {
 				countOc++;
 			}
-			if (opcao == 2 && idade > 40) {
+			if (opcao == '2' && idade > 40) {
 				countNq++;
 			}
-			if (opcao == 1 && idade < 18) {
+			if (opcao == '1' && idade < 18) {
 				countCd++;
 			}
 			
+			System.out.printf("\nContinua S/N: ");
+			continua = ((sc.next().toUpperCase().charAt(0) == 'S')? true: false);
 			count++;
 		}
-		System.out.printf("\nO número de pessoas calmas: %d", countCalm);
-		System.out.printf("\nO número de mulheres nervosas: %d", countMn);
-		System.out.printf("\nO número de homens agressivos: %d", countHa);
+		System.out.printf("\nO número de pessoas calmas: %d", countC);
+		System.out.printf("\nO número de mulheres nervosas: %d", countFn);
+		System.out.printf("\nO número de homens agressivos: %d", countMa);
 		System.out.printf("\nO número de outros calmos: %d", countOc);
 		System.out.printf("\nO número de pessoas nervosas com mais de 40 anos: %d", countNq);
 		System.out.printf("\nO número de pessoas calmas com menos de 18 anos: %d", countCd);
-		
-		
 		sc.close();
 	}
 }
