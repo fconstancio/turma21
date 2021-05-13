65a60dd55834a4d1c290c9562a91d16ea99cd30c
fconstancio <constanciosalomao@gmail.com>
2021-05-13 17:02:00 -0300

    atualização erroarquivo

--- java/AULAS/src/exemplos/ErroArquivo.java
+++ java/AULAS/src/exemplos/ErroArquivo.java
@@ -1,11 +1,32 @@
 package exemplos;
 
-import java.io.IOError;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ErroArquivo {
 	public static void main(String[] args) {
-	
+		File arquivo = new File("C:\\temp\\lista.txt.txt");
+		Scanner leia = null;
+		try {
+			leia = new Scanner(arquivo);
+			while (leia.hasNextLine()) {
+				System.out.println(leia.nextLine());
+			}
+			
+		}
+		catch ( IOException erro) {
+			System.out.println("Erro na abertura do arquivo!!!");
+			//erro.printStackTrace();
+		}
+		finally {
+			if (leia !=null) {
+				leia.close();
+			}
+		}
+		System.out.println("FIM DE PROGRAMA!!!");
+		
+		
 		
 	}
 }
