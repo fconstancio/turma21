71d547cf2e6db895cab391154beac82552aeecac
fconstancio <constanciosalomao@gmail.com>
2021-05-14 17:21:21 -0300

    termino do nível básico

--- java/AULAS/src/exemplos/cadfunc/CadFuncionario.java
+++ java/AULAS/src/exemplos/cadfunc/CadFuncionario.java
@@ -1,11 +1,9 @@
 package exemplos.cadfunc;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.Set;
 
 
 public class CadFuncionario {
